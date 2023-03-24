54306eb8575e7e4ea3ff161a436ba084f83db699
AlekseyShaykin <alekseyshaykin2311@gmail.com>
2023-03-24 14:08:04 +0700

    Open-closed principle соблюден. Рефакторинга делать не пришлось

--- src/main/java/org/example/DataService.java
+++ src/main/java/org/example/DataService.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 public interface DataService {
 
+    //  2)  Open-closed principle. Соблюден, т.к. есть базовый класс-интерфейс DataService с базовыми методами.
+    //      Другой класс UserSrvice импелметит интрефейс и меняет "базовые" методы, так как ему нужно.
+
+
     User create(Student student);
     User create(Teacher teacher);
     List<User> read(List<User> users);
